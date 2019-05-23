93f426f59e9c2db1809d84b5163e1c2b416b7eda
user1 <user1@KH_I>
2019-05-23 09:13:10 +0900

    d

---
+++ JavaSwingProject/src/com/boram/member/run/Run.java
@@ -0,0 +1,12 @@
+package com.boram.member.run;
+
+import com.boram.member.view.MemberView;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		new MemberView().mainmenu();
+	}
+
+}
--- JavaSwingProject/src/com/boram/member/view/MemberView.java
+++ JavaSwingProject/src/com/boram/member/view/MemberView.java
@@ -1,14 +1,46 @@
 package com.boram.member.view;
 
+import java.util.Scanner;
+
+import com.boram.member.controller.MemberController;
+
 public class MemberView {
+	
+	private Scanner sc = new Scanner(System.in);
+	
+	private MemberController mc = new MemberController();
 	
 	
 	public void mainmenu() {
 		
 
 	}
+	
 	public void join() {
 		
+		System.out.println("=== 회원가입 ===");
+		
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		
+		System.out.println("나이 : ");
+		int age = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("아이디 : ");
+		String id = sc.nextLine();
+		
+		System.out.println("비밀번호 : ");
+		String pwd = sc.nextLine();
+		
+		System.out.println("전화번호 : ");
+		String phone = sc.nextLine();
+		
+		System.out.println("주소 : ");
+		String address = sc.nextLine();
+		
+		System.out.println("이메일 : ");
+		String email = sc.nextLine();
 
 	}
 	
