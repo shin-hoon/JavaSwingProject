ab5dbdf4ec07d9acde4a215c039a01af3d00bc1d
fish <fish@DESKTOP-T4H9JF7>
2019-05-24 22:48:44 +0900

    패키지 정리 3차

--- src/com/boram/main/run/Run.java
+++ src/com/boram/main/run/Run.java
@@ -4,8 +4,9 @@ import com.boram.shopping.view.Test;
 
 public class Run {
 	public static void main(String[] args) {
-//		MainPage main = new MainPage(); 얘가 메인 
-		Test test = new Test();
+//		MainPage main = new MainPage(); 메인 호출 작업중
+		
+		Test test = new Test(); // 테스트용(windows builder)
 //		Test2 test2 = new Test2(); 
 	}
 }
