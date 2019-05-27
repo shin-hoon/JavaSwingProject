614b7eb7ec493295bfcf4a9e692e3525316d71c0
ffij2 <ffij2@DESKTOP-2Q7ARVH>
2019-05-27 11:11:52 +0900

    종민

---
+++ src/com/boram/manager/run/Run.java
@@ -0,0 +1,15 @@
+package com.boram.manager.run;
+
+import com.boram.manager.view.ManagerView;
+
+//юс╫ц
+
+public class Run {
+	
+	public static void main(String[] agrs) {
+		
+		new ManagerView().mainMenu();
+		
+	}
+
+}
